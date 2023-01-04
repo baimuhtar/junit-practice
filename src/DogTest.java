341f7cfd803c97568611887d84471f7ecb314a55
baimuhtar <baimuhtar1997@gmail.com>
2023-01-04 10:03:37 +0600

    changing dogTest

--- src/DogTest.java
+++ src/DogTest.java
@@ -1,19 +1,28 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DogTest {
+    static Dog dog;
+
+    //    @BeforeEach //starting before another test each time
+//    void prepareData(){
+//        dog = new Dog("Alberts", 2);
+//    }
+    @BeforeAll // running one time before all tests
+    static void prepareData() {
+        dog = new Dog("Albert", 2);
+    }
 
     @Test
     void testGetDogNameMethod() {
-        Dog dog = new Dog("Albert", 2);
-
         assertEquals("Albert", dog.getName());
     }
 
     @Test
     void testSetDogNameMethod() {
-        Dog dog = new Dog("Albert", 3);
         dog.setName("Roman");
         assertEquals("Albert", dog.getName());
     }
