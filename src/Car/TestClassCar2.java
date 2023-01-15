e4339ba1be1a32711eb47df1ee25a60b9aa0ee0b
baimuhtar <baimuhtar1997@gmail.com>
2023-01-15 13:53:52 +0600

    Parallel tests with annotation or file

--- src/Car/TestClassCar2.java
+++ src/Car/TestClassCar2.java
@@ -2,6 +2,8 @@ package Car;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,9 +13,25 @@ public class TestClassCar2 {
     @DisabledOnOs(OS.MAC)
     @EnabledOnJre(JRE.JAVA_17)
     @DisabledOnJre({JRE.JAVA_11, JRE.JAVA_12})
-    public void getCarYear(){
+
+    public void getCarYear() {
         Car2 car = new Car2(2000, "blue", 2020, 240);
         assertEquals(2020, car.getYear());
-
     }
+
+    @Test
+
+    public void getCarWeight() {
+        Car2 car = new Car2(2000, "blue", 2020, 240);
+        assertEquals(2000, car.getWeight());
+    }
+
+    @Test
+
+    public void getCarSpeed() {
+        Car2 car = new Car2(2000, "blue", 2020, 240);
+        assertEquals(240, car.getMaxSpeed());
+    }
+
+
 }
